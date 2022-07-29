3f9bbbedc613983bbc99a2932988baea64ba549e
cwd <3255375187@qq.com>
2022-07-29 20:38:44 +0800

    第一次提交,初始化项目

---
+++ HelloProject/Demo1/src/Pack1/HelloWorld.java
@@ -0,0 +1,15 @@
+package Pack1;
+/**
+ * @author chenweidong
+ * @date 2022/7/29 20:36
+ * TODO:
+ */
+
+/**
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
